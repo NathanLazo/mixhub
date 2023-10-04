8265776330342f37d16a97506a9e9bf8ceda4d27
Nathan Lazo <nathanlazo@outlook.com>
2023-10-03 19:16:15 -0600

    v0.0.1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "tailwindui.com",
+      "images.unsplash.com",
+    ],
+  },
+});
