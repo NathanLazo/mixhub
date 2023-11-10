83991dca22a8e067be09525517a7d97351066151
Jorge Montañez <jemg2510@gmail.com>
2023-11-10 10:17:13 -0600

    Add @vercel/analytics package and implement in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import { type Metadata } from "next";
 
 import { RootLayout } from "~/components/RootLayout";
@@ -20,7 +22,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className='h-full bg-neutral-950 text-base antialiased'>
       <body className='flex min-h-full flex-col'>
-        <RootLayout>{children}</RootLayout>
+        <RootLayout>
+          {children}
+          <Analytics />
+        </RootLayout>
       </body>
     </html>
   );
